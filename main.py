1efbe2bf06b4c2e3a4fa73b834e3ba67aff5eebf
Phillip-Obermueller <p.obermueller5@gmail.com>
2025-05-17 14:58:07 +0200

    FIX url paramter handling

--- main.py
+++ main.py
@@ -14,11 +14,10 @@ from logic.decline import decline
 st.set_page_config(page_title="Rabattoptionen", layout="wide")
 
 # Abrufen der aktuellen URL und Extrahieren des Hash-Codes
-url = st_javascript("await fetch('').then(r => window.parent.location.href)") 
+url = str(st_javascript("await fetch('').then(r => window.parent.location.href)")) 
 hash_code = url.split("/")[-1]
 decision = url.split("/")[-2]
-hash_code = 'diuekwfgbk3rwegb24'  # Temporärer Wert für den Hash-Code
-  
+
 # Erstellen des Supabase-Clients
 client = create_supabase_client()
 
